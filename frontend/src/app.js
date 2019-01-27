3e97560d49490807c5a2c6ec446ae505990c319f
Filip Fabiánek <fabianekphilip@gmail.com>
2019-01-27 15:35:19 +0100

    Add webpack, babel, react to frontend, add start and build commands to package.json

---
+++ frontend/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Stuff = () => (
+  <div>
+    <h1>Django-React boilerplate</h1>
+  </div>
+);
+
+ReactDOM.render(
+  <Stuff />,
+  document.getElementById("app")
+);
---
+++ frontend/webpack.config.js
@@ -0,0 +1,92 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const devMode = process.env.NODE_ENV !== 'production'
+
+module.exports = (env) => {
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        // {
+        //   test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        //   loaders: [
+        //     {
+        //       loader: "file-loader",
+        //       options: {
+        //         hash: "sha512",
+        //         name: "[hash].[ext]",
+        //         outputPath: "../assets/compiled"
+        //       }
+        //     },
+        //     {
+        //       loader: "image-webpack-loader",
+        //       query: {
+        //         mozjpeg: {
+        //           progressive: true,
+        //         },
+        //         gifsicle: {
+        //           interlaced: false,
+        //         },
+        //         optipng: {
+        //           optimizationLevel: 4,
+        //         },
+        //         pngquant: {
+        //           quality: '75-90',
+        //           speed: 3,
+        //         }
+        //       }
+        //     }
+        //   ]
+        // }
+      ]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      historyApiFallback: true,
+      port: 3000,
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('development')
+      })
+    ],
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true,
+        }),
+        new OptimizeCSSAssetsPlugin({}),
+      ]
+    },
+  };
+}
