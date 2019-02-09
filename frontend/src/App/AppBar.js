879bbd200187b073e43f206a6c91bac6aa64efd9
Filip Fabiánek <fabianekphilip@gmail.com>
2019-02-09 14:57:20 +0100

    Adjust AppBar with account icon that opens Menu on click which contains Logout, Login and Signup MenuItems

--- frontend/src/App/AppBar.js
+++ frontend/src/App/AppBar.js
@@ -9,15 +9,19 @@ import { connect } from "react-redux";
 import { logoutUser } from "../actions/user";
 
 // Material-UI
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MoreIcon from '@material-ui/icons/MoreVert';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 import { fade } from '@material-ui/core/styles/colorManipulator';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 
 import { isEmpty } from "../utils/object";
@@ -47,10 +51,11 @@ const styles = theme => ({
     '&:hover': {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
+    marginRight: theme.spacing.unit * 2,
     marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit,
+      marginLeft: theme.spacing.unit * 3,
       width: 'auto',
     },
   },
@@ -75,77 +80,159 @@ const styles = theme => ({
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-      width: 120,
+      width: 150,
       '&:focus': {
         width: 200,
       },
     },
   },
-  logoutButton: {
-    color: "#fff",
-    letterSpacing: ".5px",
-    margin: "0 .5rem 0 1.5rem",
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  menuItem: {
+    width: '90px',
   },
 });
 
-const SearchAppBar = (props) => {
-  const { classes } = props;
+class SearchAppBar extends React.Component {
+  state = {
+    anchorEl: null,
+    mobileMoreAnchorEl: null,
+  };
 
-  const logout = () => {
+  handleProfileMenuOpen = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleMenuClose = () => {
+    this.setState({ anchorEl: null });
+    this.handleMobileMenuClose();
+  };
+
+  handleMobileMenuOpen = event => {
+    this.setState({ mobileMoreAnchorEl: event.currentTarget });
+  };
+
+  handleMobileMenuClose = () => {
+    this.setState({ mobileMoreAnchorEl: null });
+  };
+
+  onLogout = () => {
     axios.post("/api_auth/logout")
     .then((res) => {
-      props.logoutUser();
-      props.history.push("/login");
+      this.handleMenuClose();
+      this.props.logoutUser();
+      this.props.history.push("/login");
     })
   };
 
-  const isLoggedIn = !isEmpty(props.user);
+  render() {
+    const { classes } = this.props;
+    const { anchorEl, mobileMoreAnchorEl } = this.state;
+    const isMenuOpen = !!anchorEl;
+    const isMobileMenuOpen = !!mobileMoreAnchorEl;
+    const isLoggedIn = !isEmpty(this.props.user);
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
-            <MenuIcon />
-          </IconButton>
-          <Link to="/">
-            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
-              {props.title}
-            </Typography>
-          </Link>
-          <div className={classes.grow} />
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
+    const renderMenu = (type) => {
+      const isMobile = type === 'mobile';
+      const anchorElement = isMobile ? mobileMoreAnchorEl : anchorEl;
+      const isOpen = isMobile ? isMobileMenuOpen : isMenuOpen;
+
+      return (
+        <Menu
+          anchorEl={anchorElement}
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          disableAutoFocusItem
+          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+          transitionDuration={{
+            enter: 500,
+            exit: 0,
+          }}
+          open={isOpen}
+          onClose={this.handleMenuClose}
+        >
+          {isLoggedIn && (
+            <MenuItem
+              className={classes.menuItem}
+              onClick={this.onLogout}
+            >Logout</MenuItem>
+          )}
+          {!isLoggedIn && (
+            <MenuItem
+              className={classes.menuItem}
+              component={Link}
+              onClick={this.handleMenuClose}
+              to="/login"
+            >Login</MenuItem>
+          )}
+          {!isLoggedIn && (
+            <MenuItem
+              className={classes.menuItem}
+              component={Link}
+              onClick={this.handleMenuClose}
+              to="/signup"
+            >Sign up</MenuItem>
+          )}
+        </Menu>
+      );
+    };
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+              <MenuIcon />
+            </IconButton>
+            <Link to="/">
+              <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+                {this.props.title}
+              </Typography>
+            </Link>
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+              />
             </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-            />
-          </div>
-          {
-            isLoggedIn ? (
-              <Button
-                className={classes.logoutButton}
-                onClick={logout}
+            <div className={classes.sectionDesktop}>
+              <IconButton
+                aria-owns={isMenuOpen ? 'material-appbar' : undefined}
+                aria-haspopup="true"
+                onClick={this.handleProfileMenuOpen}
+                color="inherit"
               >
-                Logout
-              </Button>
-            ) : (
-              <Link to="/login">
-                <Button className={classes.logoutButton}>
-                  Login
-                </Button>
-              </Link>
-            )
-          }
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+                <AccountCircle />
+              </IconButton>
+            </div>
+            <div className={classes.sectionMobile}>
+              <IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
+                <MoreIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        {renderMenu('desktop')}
+        {renderMenu('mobile')}
+      </div>
+    );
+  };
 };
 
 SearchAppBar.propTypes = {
