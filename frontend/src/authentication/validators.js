a3b9ddf7046c58511632c64f129f307c8c5f7f04
Filip Fabiánek <fabianekphilip@gmail.com>
2019-02-09 20:43:27 +0100

    Fix some authentication buggs

--- frontend/src/authentication/validators.js
+++ frontend/src/authentication/validators.js
@@ -7,7 +7,7 @@ import {
   isString,
 } from "../utils/commonValidators";
 
-export const validateUserName = val => isString(val) && isAlphanumeric(val) && minLength(val, 2) && maxLength(val, 30);
+export const validateUserName = val => isString(val) && isAlphanumeric(val) && val.trim().indexOf(' ') === -1 && minLength(val, 2) && maxLength(val, 30);
 export const validateEmail = val => validator.isEmail(val);
 export const validatePassword = val => isString(val) && minLength(val, 8) && isString(val) && maxLength(val, 50);
 
